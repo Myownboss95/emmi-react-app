0180f4120ffd776c14e3a53182962948e86b79d3
Xpressglobe <89937686+Xpressglobe@users.noreply.github.com>
2022-07-26 18:10:55 +0100

    first commit

---
+++ src/home.js
@@ -0,0 +1,46 @@
+//import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ContentColumn from './contentColumn';
+// import Card from 'react-bootstrap/Card'
+// import { Row, Col} from 'react-bootstrap'
+
+const HomeComponent = () => {
+
+    // let styleHome = {
+    //     border : '1px solid black'
+    // };
+    // let name = "Jason";
+    return ( 
+        <div>
+            
+                <Row className='container-fluid mt-5'>
+                    <Col xs={12} md={4} lg={3}>
+                        <div className='p-3'>
+                           <ContentColumn></ContentColumn>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={4} lg={3}>
+                        <div className='p-3'>
+                           <ContentColumn></ContentColumn>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={4} lg={3}>
+                        <div className='p-3'>
+                           <ContentColumn></ContentColumn>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={4} lg={3}>
+                        <div className='p-3'>
+                           <ContentColumn></ContentColumn>
+                        </div>
+                    </Col>
+                   
+                </Row>
+                
+            
+        </div>
+     );
+}
+ 
+export default HomeComponent;
